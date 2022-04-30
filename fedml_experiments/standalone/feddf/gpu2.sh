742378b7cfb85b8b439aa9d0427ef9225b137848
nakyilkim <nakyilkim@naver.com>
2022-04-30 22:59:34 +0900

    add baseline exp

---
+++ fedml_experiments/standalone/feddf/gpu2.sh
@@ -0,0 +1,12 @@
+for PART_NUM in 2 4 10
+do
+    echo ${PART_NUM}
+    EPOCH=5
+    python ./main_feddf.py --gpu 2 --client_num_in_total 20 --client_num_per_round ${PART_NUM} \
+    --batch_size 64 --dataset cifar10 --data_dir  /input/dataset/cifar10 \
+    --model resnet8_no_bn --partition_method hetero --partition_alpha 0.01 --comm_round 100 \
+     --epochs ${EPOCH} --lr 0.1 --client_optimizer sgd --frequency_of_the_test 1 --ci 1 \
+    --valid_ratio 0.1 --unlabeled_data_dir /input/dataset/cifar100 \
+     --unlabeled_dataset cifar100 --unlabeled_batch_size 128 --pname feddf
+    
+done
